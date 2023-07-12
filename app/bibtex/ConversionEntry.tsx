7897f08778af459538337e6024cc9347ed55e053
Luyu Cheng <chengluyu@live.cn>
2023-07-12 17:17:30 +0800

    feat(bibtex): support multiple files and polish UI

--- app/bibtex/ConversionEntry.tsx
+++ app/bibtex/ConversionEntry.tsx
@@ -3,67 +3,160 @@ import { Noto_Sans_Mono as NotoSansMono } from "next/font/google";
 
 const notoSansMono = NotoSansMono({ subsets: ["latin"] });
 
-export type ConversionEntry = {
-  fileName: string;
-  content: string;
-  pending: boolean;
-};
+export type ConversionEntryAction = "archive" | "delete";
+
+export type ConversionEntry =
+  | {
+      fileName: string;
+      content: null;
+      source: string;
+      status: "pending";
+    }
+  | {
+      fileName: string;
+      content: string;
+      source: string;
+      status: "converting" | "done";
+    };
 
 export type ConversionResultProps = {
   entry: ConversionEntry;
-  showArchiveButton?: boolean;
-  onArchive?: (entry: ConversionEntry) => void;
   size?: "sm" | "md" | "lg";
+  actions?: ConversionEntryAction[];
+  onAction?: (action: ConversionEntryAction, entry: ConversionEntry) => void;
 };
 
 export default function ConversionResult({
   entry,
-  showArchiveButton = false,
-  onArchive,
   size = "md",
+  actions = [],
+  onAction,
 }: ConversionResultProps) {
   return (
     <div
       key={entry.fileName}
       className={classNames(
         "flex flex-col border",
-        entry.pending
-          ? "bg-amber-100 border-amber-600"
-          : "bg-stone-100 border-stone-600",
+        {
+          "bg-amber-100 border-amber-600": entry.status === "converting",
+          "bg-neutral-100 border-sky-600": entry.status === "pending",
+          "bg-stone-100 border-stone-600": entry.status === "done",
+        },
         { "h-64": size === "md", "h-48": size === "sm", "h-96": size === "lg" }
       )}
     >
       <header
         className={classNames(
           "px-2 py-0.5 flex-shrink-0 flex items-center justify-between",
-          entry.pending
-            ? "bg-amber-600 text-amber-50"
-            : "bg-stone-600 text-stone-50"
+          {
+            "bg-amber-600 text-amber-50": entry.status === "converting",
+            "bg-sky-600 text-sky-50": entry.status === "pending",
+            "bg-stone-600 text-stone-50": entry.status === "done",
+          }
         )}
       >
-        <div className="text-lg font-bold">
-          {entry.fileName} {entry.pending ? "(pending)" : null}
+        <div className="font-bold">
+          {entry.fileName}{" "}
+          {entry.status === "done" ? null : `(${entry.status})`}
         </div>
-        {entry.pending || !showArchiveButton ? null : (
-          <button
-            onClick={onArchive ? onArchive.bind(null, entry) : undefined}
-            className="px-1 py-0.5 bg-gray-400 text-white rounded mb-2"
-          >
-            Archive
-          </button>
-        )}
       </header>
       <main className="px-3.5 py-2 flex-1 overflow-y-auto">
         <pre
           className={classNames(
             "text-sm whitespace-pre-wrap",
             notoSansMono.className,
-            { "select-none": entry.pending }
+            { "select-none": entry.status === "pending" }
           )}
         >
-          {entry.content}
+          {entry.status === "pending" ? entry.source : entry.content}
         </pre>
       </main>
+      {actions.length === 0 ? null : (
+        <footer
+          className={classNames(
+            "px-1 py-1 flex-shrink-0 flex items-center gap-1 border-t",
+            {
+              "bg-amber-200 border-amber-600": entry.status === "converting",
+              "bg-sky-100 border-sky-600": entry.status === "pending",
+              "bg-stone-200 border-stone-600": entry.status === "done",
+            }
+          )}
+        >
+          {actions.map((action) => (
+            <ConversionEntryActionButton
+              key={action}
+              entry={entry}
+              action={action}
+              onAction={onAction}
+            />
+          ))}
+        </footer>
+      )}
     </div>
   );
+}
+
+export type ConversionEntryActionButtonProps = {
+  entry: ConversionEntry;
+  action: ConversionEntryAction;
+  onAction?: (action: ConversionEntryAction, entry: ConversionEntry) => void;
+};
+
+function ConversionEntryActionButton({
+  entry,
+  action,
+  onAction,
+}: ConversionEntryActionButtonProps) {
+  switch (entry.status) {
+    case "pending": {
+      switch (action) {
+        case "delete":
+          return (
+            <button
+              key={action}
+              className="flex-shrink-0 px-2 py-0.5 bg-rose-600 text-white disabled:bg-red-600s"
+              type="button"
+              onClick={onAction?.bind(null, "delete", entry)}
+            >
+              Cancel
+            </button>
+          );
+        default:
+          return null;
+      }
+    }
+    case "converting": {
+      return null;
+    }
+    case "done": {
+      switch (action) {
+        case "archive":
+          return (
+            <button
+              key={action}
+              className="flex-shrink-0 px-2 py-0.5 bg-stone-600 text-white disabled:bg-stone-600s"
+              type="button"
+              onClick={onAction?.bind(null, "archive", entry)}
+            >
+              Archive
+            </button>
+          );
+        case "delete":
+          return (
+            <button
+              key={action}
+              className="flex-shrink-0 px-2 py-0.5 bg-rose-600 text-white disabled:bg-red-600s"
+              type="button"
+              onClick={onAction?.bind(null, "delete", entry)}
+            >
+              Delete
+            </button>
+          );
+        default:
+          return null;
+      }
+    }
+    default:
+      return null;
+  }
 }
--- app/bibtex/page.tsx
+++ app/bibtex/page.tsx
@@ -1,10 +1,41 @@
 "use client";
 
-import React, { useState } from "react";
-import { ChatGPTMessage } from "@/utils/ChatGPTMessage";
+import React, { useCallback, useEffect, useState } from "react";
 import ConversionResult, { ConversionEntry } from "./ConversionEntry";
 import { useLocalStorage } from "usehooks-ts";
 import PageHeader from "@/components/PageHeader";
+
+function readFile(file: File): Promise<ConversionEntry> {
+  return new Promise<ConversionEntry>((resolve, reject) => {
+    const reader = new FileReader();
+    let hasRead = false;
+    reader.onload = (e) => {
+      const content = e.target?.result;
+      if (typeof content === "string") {
+        resolve({
+          fileName: file.name,
+          source: content,
+          content: null,
+          status: "pending",
+        });
+      } else {
+        reject(new Error(`Cannot read content of file ${file.name}`));
+      }
+    };
+    reader.readAsText(file);
+    setTimeout(() => {
+      if (hasRead) {
+        return;
+      } else {
+        reject(new Error(`Failed to read file ${file.name}: timed out`));
+      }
+    }, 1000);
+  });
+}
+
+function readFiles(files: File[]): Promise<ConversionEntry[]> {
+  return Promise.all(files.map(readFile));
+}
 
 export default function Page() {
   const [entries, setEntries] = useLocalStorage<ConversionEntry[]>(
@@ -15,28 +46,82 @@ export default function Page() {
     "archive",
     []
   );
+  const [pendingEntries, setPendingEntries] = useLocalStorage<
+    ConversionEntry[]
+  >("pending", []);
+  const [ongoingTask, setOngoingTask] = useState<ConversionEntry | null>(null);
   const [copySuccess, setCopySuccess] = useState<string>("");
-  const [pendingTask, setPendingTask] = useState<ConversionEntry | null>(null);
+  const [isReadingFiles, setIsReadingFiles] = useState<boolean>(false);
 
-  const onFileChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    const file = event.target.files?.[0];
-    if (!file) {
+  const onFileChange = async (event: React.ChangeEvent<HTMLInputElement>) => {
+    setIsReadingFiles(false);
+    const entries = await readFiles(Array.from(event.target.files ?? []));
+    setPendingEntries(entries);
+    setIsReadingFiles(true);
+  };
+
+  const run = useCallback(
+    async (head: ConversionEntry, tail: ConversionEntry[]) => {
+      setOngoingTask({ ...head, content: "", status: "converting" });
+      try {
+        const response = await fetch("/bibtex/chat", {
+          method: "POST",
+          headers: { "Content-Type": "application/json" },
+          body: JSON.stringify({
+            model: "gpt-4",
+            messages: [{ role: "user", content: head.source }],
+          }),
+        });
+
+        if (!response.ok) {
+          setOngoingTask(null);
+          throw new Error(response.statusText);
+        }
+
+        // This data is a ReadableStream
+        const data = response.body;
+        if (!data) {
+          throw new Error("The data is not a ReadableStream");
+        }
+
+        const reader = data.getReader();
+        const decoder = new TextDecoder();
+
+        let lastMessage = "";
+        for (let done = false; !done; ) {
+          const { value, done: doneReading } = await reader.read();
+          done = doneReading;
+          lastMessage = lastMessage + decoder.decode(value);
+          setOngoingTask({
+            ...head,
+            content: lastMessage,
+            status: "converting",
+          });
+        }
+
+        setPendingEntries(tail);
+        setEntries((entries) => [
+          { ...head, content: lastMessage, status: "done" },
+          ...entries,
+        ]);
+      } catch (error) {
+      } finally {
+        setOngoingTask(null);
+      }
+    },
+    []
+  );
+
+  useEffect(() => {
+    if (ongoingTask !== null) {
       return;
     }
-
-    const reader = new FileReader();
-    reader.onload = async (e) => {
-      const content = e.target?.result as string;
-      const result = await sendMessage(file.name, { role: "user", content });
-      if (result) {
-        setEntries((output) => [
-          ...output,
-          { fileName: file.name, content: result, pending: false },
-        ]);
-      }
-    };
-    reader.readAsText(file);
-  };
+    if (pendingEntries.length === 0) {
+      return;
+    }
+    const [head, ...tail] = pendingEntries;
+    run(head, tail);
+  }, [pendingEntries]);
 
   const onCopyClick = async () => {
     try {
@@ -50,52 +135,6 @@ export default function Page() {
     }
   };
 
-  const sendMessage = async (fileName: string, message: ChatGPTMessage) => {
-    console.log("Sending message to edge function.");
-
-    setPendingTask({ fileName, content: "", pending: true });
-
-    const response = await fetch("/chat", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({
-        model: "gpt-4",
-        messages: [message],
-      }),
-    });
-
-    if (!response.ok) {
-      throw new Error(response.statusText);
-    }
-
-    // This data is a ReadableStream
-    const data = response.body;
-    if (!data) {
-      return null;
-    }
-
-    const reader = data.getReader();
-    const decoder = new TextDecoder();
-    let done = false;
-
-    let lastMessage = "";
-
-    while (!done) {
-      const { value, done: doneReading } = await reader.read();
-      done = doneReading;
-      const chunkValue = decoder.decode(value);
-
-      lastMessage = lastMessage + chunkValue;
-
-      setPendingTask({ fileName, content: lastMessage, pending: true });
-    }
-
-    setPendingTask(null);
-    return lastMessage;
-  };
-
   return (
     <>
       <PageHeader />
@@ -105,7 +144,8 @@ export default function Page() {
           className="border border-stone-400"
           type="file"
           onChange={onFileChange}
-          disabled={pendingTask !== null}
+          disabled={isReadingFiles}
+          multiple
         />
         {copySuccess && <div className="mb-2">{copySuccess}</div>}
       </div>
@@ -113,37 +153,73 @@ export default function Page() {
         <section className="flex-1 min-w-0 flex flex-col gap-2 px-3.5 pt-1 pb-2 border-r-2 border-stone-400">
           <header className="h-10 flex-shrink-0 flex items-center justify-between border-b border-dashed border-stone-600">
             <h2 className="text-lg font-bold">Results ({entries.length})</h2>
-            <button
-              className="flex-shrink-0 px-2 h-8 bg-blue-600 text-white disabled:bg-stone-600s"
-              type="button"
-              onClick={onCopyClick}
-            >
-              Copy ALL in the workbench
-            </button>
+            <div className="flex items-center gap-2">
+              <button
+                className="flex-shrink-0 px-2 h-8 bg-stone-600 text-white disabled:bg-stone-600s"
+                type="button"
+                onClick={onCopyClick}
+              >
+                Archive all
+              </button>
+              <button
+                className="flex-shrink-0 px-2 h-8 bg-blue-600 text-white disabled:bg-stone-600s"
+                type="button"
+                onClick={onCopyClick}
+              >
+                Copy all
+              </button>
+            </div>
           </header>
-          <div className="flex-1 min-h-0 grid grid-cols-3 gap-4">
-            {pendingTask === null ? null : (
-              <ConversionResult entry={pendingTask} size="lg" />
-            )}
-            {entries.map((o) => (
-              <ConversionResult
-                key={o.fileName}
-                entry={o}
-                showArchiveButton
-                onArchive={(x) => {
-                  setEntries(entries.filter((e) => e.fileName !== x.fileName));
-                  setArchive([...archive, x]);
-                }}
-                size="md"
-              />
-            ))}
-          </div>
+          <main className="flex-1 min-h-0 overflow-y-auto">
+            <div className="flex-1 min-h-0 grid grid-cols-2 gap-4">
+              {ongoingTask === null ? null : (
+                <ConversionResult entry={ongoingTask} size="lg" />
+              )}
+              {pendingEntries.map((o) =>
+                o.fileName === ongoingTask?.fileName ? null : (
+                  <ConversionResult
+                    key={o.fileName}
+                    entry={o}
+                    size="sm"
+                    actions={["delete"]}
+                    onAction={(action, entry) => {
+                      if (action === "delete") {
+                        setEntries(
+                          pendingEntries.filter(
+                            (e) => e.fileName !== entry.fileName
+                          )
+                        );
+                      }
+                    }}
+                  />
+                )
+              )}
+              {entries.map((o) => (
+                <ConversionResult
+                  key={o.fileName}
+                  entry={o}
+                  actions={["archive", "delete"]}
+                  onAction={(action, entry) => {
+                    if (action === "archive") {
+                      setEntries(
+                        entries.filter((e) => e.fileName !== entry.fileName)
+                      );
+                      setArchive([...archive, entry]);
+                    } else if (action === "delete") {
+                      setEntries(
+                        entries.filter((e) => e.fileName !== entry.fileName)
+                      );
+                    }
+                  }}
+                  size="md"
+                />
+              ))}
+            </div>
+          </main>
         </section>
         <section className="flex-1 min-w-0 flex flex-col gap-2 px-3.5 pt-1 pb-2">
           <header className="h-10 flex-shrink-0 flex items-center justify-between border-b border-dashed border-stone-600">
-            <h2 className="text-lg font-bold">
-              Archive ({archive.length})
-            </h2>
+            <h2 className="text-lg font-bold">Archive ({archive.length})</h2>
             {/* <button
               className="flex-shrink-0 px-2 h-8 bg-blue-600 text-white disabled:bg-stone-600s"
               type="button"
