41e68af90f6c06613631ee14c4212d0f5552296d
Luyu Cheng <chengluyu@live.cn>
2023-07-12 17:23:57 +0800

    feat(home): show README

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,33 @@
-import Link from "next/link";
+import PageHeader from "@/components/PageHeader";
+import { promises as fs } from "fs";
+import { unified } from "unified";
+import remarkParse from "remark-parse";
+import remarkGfm from "remark-gfm";
+import remarkRehype from "remark-rehype";
+import rehypeStringify from "rehype-stringify";
 
-export default function HomePage() {
+export default async function HomePage() {
+  const readMeHtml = await parseReadMe();
   return (
     <>
-      <Link href="/chat">Chat</Link>
-      <Link href="/reader">Reader Test</Link>
-      <Link href="/bibtex">BibTeX Converter</Link>
+      <PageHeader />
+      <main className="flex-1 min-h-0 overflow-y-auto">
+        <div
+          className="p-4 prose"
+          dangerouslySetInnerHTML={{ __html: readMeHtml }}
+        ></div>
+      </main>
     </>
   );
+}
+
+async function parseReadMe(): Promise<string> {
+  const readme = await fs.readFile("README.md", "utf-8");
+  const processor = unified()
+    .use(remarkParse)
+    .use(remarkGfm)
+    .use(remarkRehype)
+    .use(rehypeStringify);
+  const result = await processor.process(readme);
+  return result.toString();
 }
